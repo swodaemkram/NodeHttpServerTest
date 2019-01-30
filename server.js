7048713dfe347b76331c80b3aeec113108d2ef0e
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2019-01-30 10:37:41 -0500

    Create server.js
    Just a test node.js server

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+
+http.createServer(function(request,response) {
+    response.writeHead(200,{"Content-Type": "text/plain"});
+    response.write("Mark is Cool");
+    response.end();
+
+}).listen(8888);
